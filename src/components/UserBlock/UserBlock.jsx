61bd19cf6d109a1d7a1bb80b54c1ca844cbdf97f
unknown <serega.mironov11@gmail.com>
2021-11-09 06:43:06 +0300

    Добавляет небольшие правки стилей

--- src/components/UserBlock/UserBlock.jsx
+++ src/components/UserBlock/UserBlock.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const UserBlock = () => {
+const UserBlock = ({ isAuthUser }) => {
+
   return (
-    <div className="user-block">
-      <div className="user-block__avatar">
-        <img src="/img/avatar.jpg" alt="User avatar" width="63" height="63" />
+    isAuthUser
+      ?
+      <div className="user-block">
+        <div className="user-block__avatar">
+          <img src="/img/avatar.jpg" alt="User avatar" width="63" height="63" />
+        </div>
       </div>
-    </div>
+      :
+      <div class="user-block">
+        <a href="sign-in.html" class="user-block__link">Sign in</a>
+      </div>
   );
 };
 
